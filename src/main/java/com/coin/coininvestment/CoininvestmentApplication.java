a737a22ff5af69a9eb16b43f3fc9c62d974638a7
youngho3358 <youngho3358@gmail.com>
2024-07-26 00:25:37 +0900

    Spring Boot 서버 최초 세팅

---
+++ src/main/java/com/coin/coininvestment/CoininvestmentApplication.java
@@ -0,0 +1,13 @@
+package com.coin.coininvestment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoininvestmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoininvestmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coin/coininvestment/CoininvestmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coin.coininvestment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoininvestmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
